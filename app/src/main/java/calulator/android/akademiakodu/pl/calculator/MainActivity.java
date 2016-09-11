47fa87b6cdf540e70f484cfaf4f21fc66a2eedc8
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-11 22:41:34 +0200

    Calculator with ButterKnifeLibrary first version

--- app/src/main/java/calulator/android/akademiakodu/pl/calculator/MainActivity.java
+++ app/src/main/java/calulator/android/akademiakodu/pl/calculator/MainActivity.java
@@ -17,92 +17,153 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends Activity implements OnClickListener{
+import java.nio.BufferUnderflowException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
 
 
 
+    @BindView(R.id.num1)
     public EditText num1;
+    @BindView(R.id.num2)
     public EditText num2;
+
+    @BindView(R.id.plus)
     public Button plus;
+
+    @BindView(R.id.minus)
     public Button minus;
+
+    @BindView(R.id.divide)
     public Button divide;
+
+    @BindView(R.id.multiply)
     public Button multiply;
-    public TextView result;
-    public int n1,n2;
-    private Integer r;
+
+    @BindView(R.id.result)
+    TextView result;
+
+
+    public Double n1;
+    public Double n2;
+    public double r;
+
 
     private boolean isChanged =true;
-
-
+    private View v;
+    public double[] intValues = {0,0};
 
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
+    //private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        num1 = (EditText) findViewById(R.id.num1);
-        num2 = (EditText) findViewById(R.id.num2);
-        plus = (Button) findViewById(R.id.plus);
-        minus = (Button) findViewById((R.id.minus));
-        multiply = (Button) findViewById((R.id.multiply));
-        divide = (Button) findViewById((R.id.divide));
-        result = (TextView) findViewById(R.id.result);
+        ButterKnife.bind(this);
 
 
 
 
+       // num1 = (EditText) findViewById(R.id.num1);
+     //   num2 = (EditText) findViewById(R.id.num2);
 
-        plus.setOnClickListener(this);
-        minus.setOnClickListener(this);
-        divide.setOnClickListener(this);
-        multiply.setOnClickListener(this);
+
+     //   plus = (Button) findViewById(R.id.plus);
+    //    minus = (Button) findViewById((R.id.minus));
+     //   multiply = (Button) findViewById((R.id.multiply));
+      //  divide = (Button) findViewById((R.id.divide));
+       // result = (TextView) findViewById(R.id.result);
+
+
+
+
+
+       // plus.setOnClickListener(this);
+       // minus.setOnClickListener(this);
+      //  divide.setOnClickListener(this);
+      //  multiply.setOnClickListener(this);
+
+
+
+    }
+
+   // int[] intValues = {0,0};
+    public double[] getValueFromEditText(EditText num1,EditText num2)
+    {
+        n1 = Double.valueOf(num1.getText().toString());
+        n2 = Double.valueOf(num2.getText().toString());
+
+        intValues[0] = n1;
+        intValues[1] = n2;
+
+       return intValues;
+
+
+    }
+
+
+
+    @OnClick(R.id.plus) void plus (){
+    //   if(plus.getId() == v.getId())
+          getValueFromEditText(num1,num2);
+         r=intValues[0]+intValues[1];
+
+    }
+
+    @OnClick(R.id.minus) void minus ()
+    {
+        //if(minus.getId() == v.getId())
+            getValueFromEditText(num1,num2);
+        r=intValues[0]-intValues[1];
+
+
+
+    }
+
+    @OnClick(R.id.divide) void divide() {
+        getValueFromEditText(num1, num2);
+
+        if(intValues[0]==0 || intValues[1]==0)
+        {
+            Toast.makeText(this, " nie mozna dzielic przez zero!!", Toast.LENGTH_LONG).show();
+        }
+        else
+            r = intValues[0] / intValues[1];
 
 
 
     }
 
 
-    @Override
-    public void onClick(View v) {
+        @OnClick(R.id.multiply) void multiply() {
+
+            getValueFromEditText(num1,num2);
+            r=intValues[0]*intValues[1];
+        }
 
 
 
-       n1 = Integer.valueOf(num1.getText().toString());
-       n2 = Integer.valueOf(num2.getText().toString());
 
+    @OnClick(R.id.resultButton) void  resultButton()
+    {
 
-        if(isChanged) {
-
-            if (plus.getId() == v.getId()) {
-                r = n1 + n2;
-
-
-            } else if (minus.getId() == v.getId()) {
-                r = n1 - n2;
-            } else if (multiply.getId() == v.getId()) {
-                r = n1 * n2;
-            } else if (divide.getId() == v.getId()) {
-                r = n1 / n2;
-            }
-            result.setText(r.toString());
-
-
+            result.setText(Double.toString(r));
 
         }
 
 
 
-    }
+
 
 
 
